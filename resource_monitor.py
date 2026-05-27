cda317092b75ed82b19c26bd53f582ca6a3cbc8e
Zhuoyi04 <joeyvinji04@gmail.com>
2026-05-27 15:50:52 +0900

    Add CPU usage feature

--- resource_monitor.py
+++ resource_monitor.py
@@ -1,11 +1,19 @@
 import platform
+import psutil
 
 def main():
     print("System Resource Monitor")
     print("=" * 23)
+
     print("System:", platform.system())
     print("Release:", platform.release())
     print("Machine:", platform.machine())
+
+    print("\nCPU Information")
+    print("-" * 15)
+    print("Physical Cores:", psutil.cpu_count(logical=False))
+    print("Total Cores:", psutil.cpu_count(logical=True))
+    print("CPU Usage (%):", psutil.cpu_percent())
 
 
 if __name__ == "__main__":
